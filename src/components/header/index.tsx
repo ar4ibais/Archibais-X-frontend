fc98165b832b5d9e43f8250ad3706671afe885aa
ar4ibais <ak.hutov@yandex.ru>
2024-05-13 01:28:38 +0400

    made component for profile and made logout logic

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { ThemeContext } from "../theme-provider"
 import {
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -8,9 +9,23 @@ import {
 } from "@nextui-org/react"
 import { FaRegMoon } from "react-icons/fa"
 import { LuSunMedium } from "react-icons/lu"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { logout, selectIsAuthentificated } from "../../features/user/userSlice"
+import { useNavigate } from "react-router-dom"
+import { CiLogout } from "react-icons/ci"
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext)
+  const isAuthentificated = useAppSelector(selectIsAuthentificated)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem("token")
+    navigate("/auth")
+  }
+
   return (
     <Navbar>
       <NavbarBrand>
@@ -23,7 +38,18 @@ const Header = () => {
         >
           {theme === "light" ? <FaRegMoon /> : <LuSunMedium />}
         </NavbarItem>
-        <NavbarItem></NavbarItem>
+        <NavbarItem>
+          {isAuthentificated && (
+            <Button
+              color="default"
+              variant="flat"
+              className="gap-2"
+              onClick={handleLogout}
+            >
+              <CiLogout /> <span>Выйти</span>
+            </Button>
+          )}
+        </NavbarItem>
       </NavbarContent>
     </Navbar>
   )
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -8,6 +8,7 @@ import {
   selectUser,
 } from "../../features/user/userSlice"
 import { useEffect } from "react"
+import Profile from "../profile"
 
 const Layout = () => {
   const isAuthentificated = useAppSelector(selectIsAuthentificated)
@@ -28,6 +29,9 @@ const Layout = () => {
         </div>
         <div className="flex-1 p-4">
           <Outlet />
+        </div>
+        <div className="flex-2 p-4">
+          <div className="flex-col flex gap-5">{!user && <Profile />}</div>
         </div>
       </Container>
     </>
