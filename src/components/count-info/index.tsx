96b038befe914d5df1b14695ed5a0781a1b2ce43
ar4ibais <ak.hutov@yandex.ru>
2024-05-17 15:02:45 +0300

    made user`s profile page

---
+++ src/components/count-info/index.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  count: number
+  title: string
+}
+
+const CountInfo = ({ count, title }: Props) => {
+  return (
+    <div className="flex flex-col items-center space-x-2 p-4">
+      <span className="text-4xl font-semibold">{count}</span>
+      <span>{title}</span>
+    </div>
+  )
+}
+
+export default CountInfo
